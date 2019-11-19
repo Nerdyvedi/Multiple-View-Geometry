df7e1dfd37f12ff3023f9ae93a93a3ae132b9b55
Vedanta Jha <jhavedanta@hotmail.com>
2019-11-19 23:25:42 +0530

    Added implementation of lucas kanade method
    Python implementation of lucas kanade methof for optical flow estimation has been added

---
+++ Lucas-Kanade-Method/lkm.py
@@ -0,0 +1,34 @@
+import numpy as np
+import numpy as np
+from scipy import signal
+def optical_flow(I1g, I2g, window_size, tau=1e-2):
+ 
+    kernel_x = np.array([[-1., 1.], [-1., 1.]])
+    kernel_y = np.array([[-1., -1.], [1., 1.]])
+    kernel_t = np.array([[1., 1.], [1., 1.]])#*.25
+    w = window_size/2 # window_size is odd, all the pixels with offset in between [-w, w] are inside the window
+    I1g = I1g / 255. # normalize pixels
+    I2g = I2g / 255. # normalize pixels
+    # Implement Lucas Kanade
+    # for each point, calculate I_x, I_y, I_t
+    mode = 'same'
+    fx = signal.convolve2d(I1g, kernel_x, boundary='symm', mode=mode)
+    fy = signal.convolve2d(I1g, kernel_y, boundary='symm', mode=mode)
+    ft = signal.convolve2d(I2g, kernel_t, boundary='symm', mode=mode) +
+         signal.convolve2d(I1g, -kernel_t, boundary='symm', mode=mode)
+    u = np.zeros(I1g.shape)
+    v = np.zeros(I1g.shape)
+    # within window window_size * window_size
+    for i in range(w, I1g.shape[0]-w):
+        for j in range(w, I1g.shape[1]-w):
+            Ix = fx[i-w:i+w+1, j-w:j+w+1].flatten()
+            Iy = fy[i-w:i+w+1, j-w:j+w+1].flatten()
+            It = ft[i-w:i+w+1, j-w:j+w+1].flatten()
+            #b = ... # get b here
+            #A = ... # get A here
+            # if threshold τ is larger than the smallest eigenvalue of A'A:
+            nu = ... # get velocity here
+            u[i,j]=nu[0]
+            v[i,j]=nu[1]
+ 
+    return (u,v)
